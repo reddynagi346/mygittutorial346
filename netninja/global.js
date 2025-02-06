e50503fd730f76bc12eaade791335129b87f5510
reddynagi346 <nagigummitha346@gmail.com>
2025-02-06 16:44:35 +0530

    changes in global-2

--- netninja/global.js
+++ netninja/global.js
@@ -16,20 +16,28 @@ console.log(os.homedir());
 const http = require("http");
 const port=process.env.PORT ||5000;
 const server=http.createServer((req,res)=>{
+
  res.setHeader("Content-Type", "text/html");
+
  if(req.url==="/"){
-  res.write("This is Home page");
-  res.end();
+   res.write("This is Home page <br/>");
+   res.write(`<a href="/about">About page</a> <br/>`)
+   res.write(`<a href="/image">Image page</a> <br/>`)
+   res.end();
  }else if(req.url==="/about"){
-  res.write("This is about page");
-  res.end();
+   res.write("This is about page <br/>");
+   res.write(`<a href="/">Home page</a> <br/>`);
+   res.write(`<a href="/image">Image page</a>`)
+   res.end();
  }else if(req.url==="/image"){
-  res.write("Here is your Image");
-  res.end();
+   res.write("Here is your Image <br/>");
+   res.write(`<a href="/">Home page</a> <br/>`)
+   res.write(`<a href="/about">About Page</a>`)
+   res.end();
  }else{
-  res.write("404 Not Found");
-  res.write(` <h1> Page is not found</h1> `)
-  res.end();
+   res.write("404 Not Found");
+   res.write(` <h1> Page is not found</h1> `)
+   res.end();
  }
 })
 server.listen(port,()=>{
