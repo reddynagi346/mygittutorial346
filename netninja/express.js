5b69446f8682cb51452e67e5ee6995e5b708543c
reddynagi346 <nagigummitha346@gmail.com>
2025-02-11 12:58:37 +0530

    `Added new routes and modified existing route in netninja/express.js`

--- netninja/express.js
+++ netninja/express.js
@@ -1,13 +1,27 @@
+require('dotenv').config();
 const express= require("express");
 const app = express();
-const port =process.env.PORT || 8000
+const Blog = require("./modals/blogs")
 const morgan =require('morgan')
+const mongoose = require('mongoose');
+
+const MONGODB_URI = process.env.MONGO_URI;
+
+const port =process.env.PORT || 8000
+// const dbUri="mongodb+srv://nagireddy3464:3464nagi@cluster0.hgil2.mongodb.net/Mern-Chat-App";
+mongoose.connect(MONGODB_URI)
+.then((result)=>app.listen(port,()=>{console.log(`Mongo Db connected Successfully ${port}`);
+}))
+.catch((err)=>console.log(err))
 
 app.set('view engine','ejs')
 
 app.use(express.static('public'))
 
 app.use(morgan('dev'))
+
+
+
 
 app.get('/',(req,res,next)=>{
  res.render('index');
@@ -34,9 +48,41 @@ app.get("/blog/create",(req,res)=>{
 // res.send("Create a New Blog here")
 res.render('create')
 })
+app.get("/add-blog",(req,res)=>{
+  const blog =new Blog({
+    title:"This is a new Blog",
+    snippet:"This is the content of the blog",
+    body:"This is the body of the blog"
+  });
+  blog.save()
+  .then((result)=>{
+    res.send(result)
+})
+.catch((err)=>{
+  res.send(err)
+})
+})
+app.get("/all-blogs",(req,res)=>{
+  Blog.find()
+  .then((result)=>{
+    res.send(result)
+})
+.catch((err)=>{
+  console.log(err);
+})
+});
+app.get("/single-blog",(req,res)=>{
+  Blog.findById("67a9d32b6f4976031e50bb58")
+  .then((result)=>{
+    res.send(result)
+})
+.catch((err)=>{
+  res.send(err)
+})
+})
 app.get("/enrollment",(req,res)=>{
  res.render('information')
 })
-app.listen(port, ()=>{
- console.log(`server is running on port ${port}`);
-})
+// app.listen(port, ()=>{
+//  console.log(`server is running on port ${port}`);
+// })
--- netninja/global.js
+++ netninja/global.js
@@ -1,11 +1,11 @@
 setTimeout(()=>{
- console.log("in the timeout");
- clearInterval(int);
+console.log("in the timeout");
+clearInterval(int);
 console.log(__dirname);
 console.log(__filename);
 },3000)
 const int=setInterval(()=>{
- console.log("In the interval");
+console.log("In the interval");
 },500);
 const people =require("./persons")
 console.log(people);
@@ -16,40 +16,36 @@ console.log(os.homedir());
 const http = require("http");
 const port=process.env.PORT ||5000;
 const server=http.createServer((req,res)=>{
-
- res.setHeader("Content-Type", "text/html");
-
- if(req.url==="/"){
-   res.write("This is Home page <br/>");
-   res.write(`<a href="/about">About page</a> <br/>`)
-   res.write(`<a href="/image">Image page</a> <br/>`)
-   res.end();
- }else if(req.url==="/about"){
-   res.write("This is about page <br/>");
-   res.write(`<a href="/">Home page</a> <br/>`);
-   res.write(`<a href="/image">Image page</a>`)
-   res.end();
- }else if(req.url==="/image"){
-   res.write("Here is your Image <br/>");
-   res.write(`<a href="/">Home page</a> <br/>`)
-   res.write(`<a href="/about">About Page</a>`)
-   res.end();
- }else{
-   res.write("404 Not Found");
-   res.write(` <h1> Page is not found</h1> `)
-   res.end();
- }
+res.setHeader("Content-Type", "text/html");
+if(req.url==="/"){
+  res.write("This is Home page <br/>");
+  res.write(`<a href="/about">About page</a> <br/>`)
+  res.write(`<a href="/image">Image page</a> <br/>`)
+  res.end();}else if(req.url==="/about"){
+  res.write("This is about page <br/>");
+  res.write(`<a href="/">Home page</a> <br/>`);
+  res.write(`<a href="/image">Image page</a>`)
+  res.end();}else if(req.url==="/image"){
+  res.write("Here is your Image <br/>");
+  res.write(`<a href="/">Home page</a> <br/>`)
+  res.write(`<a href="/about">About Page</a>`)
+  res.end();
+}else{
+  res.write("404 Not Found");
+  res.write(` <h1> Page is not found</h1> `)
+  res.end();
+}
 })
 server.listen(port,()=>{
- console.log(` Server created Successfully on ${port}`);
- 
+console.log(` Server created Successfully on ${port}`);
+
 })
 
 const _= require('lodash')
- const num=_.random(0,50);
- console.log(num);
- const greet=_.once(()=>{
+const num=_.random(0,50);
+console.log(num);
+const greet=_.once(()=>{
   console.log("Hello");
- })
- greet()
+})
+greet()
 
